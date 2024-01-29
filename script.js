04905804566c2f34da8e7a9f3c651de7be1a3d61
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-29 14:20:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+let MinSum=0;
+	arr.sort((a,b)=>a-b);
+	
+	while (arr.length>1) {
+		let sum=arr[0]+arr[1];
+		arr.shift();
+		arr.shift();
+		let i=0;
+		while(arr[i]<sum){
+            i++;
+        }
+       if(i==0){
+           arr.unshift(sum);
+       }
+        else{
+            arr.splice(i,0,sum);
+        }
+		
+		//arr.sort((a,b)=>a-b);
+	MinSum+=sum;
+	}
+	
+	//console.log(MinSum);
+	return MinSum;
 }
 
 module.exports=mincost;
